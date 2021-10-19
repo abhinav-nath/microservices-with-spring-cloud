dcd1b3016edb03db5f35759bb1128de5f809ff2b
Abhinav Nath <abhinavnath@ymail.com>
2021-10-19 20:20:17 +0530

    Add initial code for limits-service

---
+++ limits-service/src/main/java/com/codecafe/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -0,0 +1,10 @@
+package com.codecafe.microservices.limitsservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+
+
+}
