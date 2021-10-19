14a1a1a1eb6164afe3daa749a3dca78d62afd13e
Abhinav Nath <abhinavnath@ymail.com>
2021-10-19 21:07:16 +0530

    Read properties from application.properties

--- limits-service/src/main/java/com/codecafe/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/codecafe/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,10 +1,25 @@
 package com.codecafe.microservices.limitsservice.controller;
 
+import com.codecafe.microservices.limitsservice.model.Configuration;
+import com.codecafe.microservices.limitsservice.model.LimitConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    private final Configuration configuration;
 
+    public LimitsConfigurationController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return LimitConfiguration.builder()
+                .minimum(configuration.getMinimum())
+                .maximum(configuration.getMaximum())
+                .build();
+    }
 
 }
