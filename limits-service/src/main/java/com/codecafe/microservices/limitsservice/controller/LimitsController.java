37fdd05a0b2a4a78e9bee401aeea185bee735624
Abhinav Nath <abhinavnath@ymail.com>
2021-10-20 22:35:19 +0530

    Add support for Actuator's Refresh endpoint

--- limits-service/src/main/java/com/codecafe/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/codecafe/microservices/limitsservice/controller/LimitsController.java
@@ -2,9 +2,11 @@ package com.codecafe.microservices.limitsservice.controller;
 
 import com.codecafe.microservices.limitsservice.model.Configuration;
 import com.codecafe.microservices.limitsservice.model.Limits;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class LimitsController {
 
